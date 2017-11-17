688431f59210aa080963b140db87ea2a508292a7
Jacky Cao <probablyameow+github@gmail.com>
2017-11-17 14:21:46 +0000

    back-up before another unit removal
    woo!

--- physics_problem_solving/sn_program/__main__.py
+++ physics_problem_solving/sn_program/__main__.py
@@ -6,7 +6,7 @@ import plotter
 """ Module to calculate the dark energy density, Omega_Lambda."""
 
 pc = 3.09 * 10 ** 16 # Parsec in m
-l_sol = 3.84 * (10**26) # Luminosity of the Sun in Watts, W
+# l_sol = 3.84 * (10**26) # Luminosity of the Sun in Watts, W
 
 file_name = 'sn_data.txt'
 cm_data = [75, (3*(10**8))] # In the order [hubbles_constant (km s^-1 Mpc^-1), speed_of_light (ms^-1)]
