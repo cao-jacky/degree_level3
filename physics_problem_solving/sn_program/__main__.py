84aa1da7e0409481bebc21742b0edca8bf1de7c5
Jacky Cao <probablyameow+github@gmail.com>
2017-11-17 19:17:37 +0000

    work done
    pps work - making more work for it

--- physics_problem_solving/sn_program/__main__.py
+++ physics_problem_solving/sn_program/__main__.py
@@ -2,6 +2,8 @@
 import data 
 import l_peak
 import omega_lambda
+import l_peak_uncert
+import omega_lambda_uncert
 import plotter
 
 """ Module to calculate the dark energy density, Omega_Lambda."""
@@ -15,9 +17,14 @@ rnge = [0,1] # Range to test Omega_Lambda values in
 
 data = data.data_input(file_name)
 
+# Finding the best fit L_peak value, and best fit Omega_Lambda value
 l_peak = l_peak.chi_sq_min(cm_data[0], cm_data[1], data, 0.01)
 omega_lambda.chi_sq_min(cm_data[0], cm_data[1], data, 0.01, float(l_peak[1]))
 
+# Finding the uncertainties on L_peak and Omega_Lambda
+l_peak_uncert.data(cm_data[0], cm_data[1], data, 0.01)
+
+# Plotting the graphs that we need
 plotter.plot_l(cm_data[0], cm_data[1], data, 0.01)
 plotter.plot_o(cm_data[0], cm_data[1], data, 0.01, float(l_peak[1]))
 plotter.plot_redmag(cm_data[0], cm_data[1], data, 0.01, float(l_peak[1]))
--- physics_problem_solving/sn_program/l_peak_uncert.py
+++ physics_problem_solving/sn_program/l_peak_uncert.py
@@ -8,6 +8,24 @@ def data(hubble, c, data, step):
     chi_sq_min = l_peak.chi_sq_min(hubble, c, data, step)[0] # Calling the value for chi_sq_min and the corresponding L_peak
     d = l_peak.chi_sq_l_peak(hubble, c, data, step) # Calling the data used to find the value of min chi^2
 
-    min_index = np.where(d[:,1] == chi_sq_min) # Finding index of min value
+    min_index = np.where(d[:,1] == chi_sq_min)[0] # Finding index of min value
 
-    print min_index
+    for i in range(20):
+        min_a = min_index + i # Finding above chi^2
+        for j in range(20):
+            min_b = min_index - j # Finding below chi^2
+            
+            chi_a = d[:,1][min_a] # Finding the corresponding chi^2 value for above
+            chi_b = d[:,1][min_b] # Finding the corresponding chi^2 value for below
+
+            #print chi_a - chi_b
+
+            if np.abs(chi_a - chi_b) >= 1:
+                print chi_a - chi_b
+                print chi_sq_min
+                print min_index
+                print min_a, min_b
+                break
+        break 
+
+
