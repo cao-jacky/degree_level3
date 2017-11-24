e7c64880597c7eac61d26d085dbe07062f8d7a0e
Jacky Cao <probablyameow+github@gmail.com>
2017-11-24 23:21:22 +0000

    galaxy subtraction program
    - trying to make it read the fits files

--- laboratory_project/programs/galaxy_subtraction/data.py
+++ laboratory_project/programs/galaxy_subtraction/data.py
@@ -1,3 +1,31 @@
 import numpy as np
 import matplotlib
-import matplib.pyplot as pyplot
+import matplotlib.pyplot as pyplot
+
+from astropy.utils.data import download_file
+from astropy.io import fits
+
+#image_file = download_file('http://data.astropy.org/tutorials/FITS-images/HorseHead.fits', cache=True )
+
+hdu_list = fits.open('amosaic.fits')
+hdu_list.info()
+
+image_data = hdu_list[0].data
+
+print(type(image_data))
+print(image_data.shape)
+
+print(image_data)
+
+np.savetxt('data.txt', image_data)
+
+hdu_list.close()
+
+pyplot.imshow(image_data, cmap='gray')
+#pyplot.colorbar()
+pyplot.savefig('amosaic2.pdf')
+
+print('Min:', np.min(image_data))
+print('Max:', np.max(image_data))
+print('Mean:', np.mean(image_data))
+print('Stdev:', np.std(image_data))
