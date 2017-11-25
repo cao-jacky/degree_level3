ca950fd2807ce70ba9ae436d8d533e9ef56eb3ea
Jacky Cao <probablyameow+github@gmail.com>
2017-11-25 19:17:41 +0000

    backup before rechange

--- laboratory_project/programs/galaxy_subtraction/data.py
+++ laboratory_project/programs/galaxy_subtraction/data.py
@@ -1,33 +1,12 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as pyplot
+import aplpy
 
-from astropy.utils.data import download_file
-from astropy.io import fits
-
-#image_file = download_file('http://data.astropy.org/tutorials/FITS-images/HorseHead.fits', cache=True )
-
-hdu_list = fits.open('amosaic.fits')
-hdu_list.info()
-
-image_data = hdu_list[0].data
-
-print(type(image_data))
-print(image_data.shape)
-
-print(len(image_data))
-
-print(image_data)
-
-np.savetxt('data.txt', image_data)
-
-hdu_list.close()
-
-pyplot.imshow(image_data, cmap='gray')
-#pyplot.colorbar()
-pyplot.savefig('amosaic2.pdf')
-
-print('Min:', np.min(image_data))
-print('Max:', np.max(image_data))
-print('Mean:', np.mean(image_data))
-print('Stdev:', np.std(image_data))
+# Using aplpy module to save the fits files into pdfs to view the images, astropy did 
+# not work at all because of the nan's
+gc = aplpy.FITSFigure('amosaic.fits')
+gc.show_grayscale()
+#gc.show_colorscale()
+#gc.add_grid()
+gc.save('test1234.pdf')
