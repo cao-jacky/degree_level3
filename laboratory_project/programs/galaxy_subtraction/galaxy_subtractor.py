990dcf91822c04c0aaaf9227abfa4c98afe3e7f3
Jacky Cao <probablyameow+github@gmail.com>
2017-12-01 20:53:59 +0000

    began galaxy subtractor program

---
+++ laboratory_project/programs/galaxy_subtraction/galaxy_subtractor.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+import sys
+sys.path.insert(0, '/Users/jackycao/Documents/Projects/degree_level3/laboratory_project/programs/aligner')
+
+import aligner # Importing aligner module from a different folder
+
+def data():
+    """ Imports data from the HD location and then copies out the data array. """
+
+    return
